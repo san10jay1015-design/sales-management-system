792fb0d3ca2f8a5ad2de79428784e1ac762c0fb3
san10jay1015-design <san10jay1015@gmail.com>
2026-04-19 02:45:53 +0530

    Update db.py

--- app/db.py
+++ app/db.py
@@ -4,7 +4,7 @@ def get_connection():
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Sanjay@10",   
+        password="*********",   
         database="sales_db"
     )
     return conn
